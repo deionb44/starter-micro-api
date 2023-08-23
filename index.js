cb2ec0c9f5a07ff8a88adecae70bb559e7eecb9a
deionb44 <120497721+deionb44@users.noreply.github.com>
2023-08-23 10:59:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const express = require('express');
+const { exec } = require('child_process');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());  // Middleware to parse JSON requests
+
+app.post('/runPython', (req, res) => {
+    const url = req.body.url;
+
+    exec(`python your_python_script.py "${url}"`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return res.status(500).send({ error: 'Failed to execute python script.' });
+        }
+        res.send({ data: stdout });
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
